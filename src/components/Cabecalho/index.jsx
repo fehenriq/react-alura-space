44513d253bf85050121e0033471580478e400a47
Felipe <lipe.h.r@gmail.com>
2022-10-19 11:33:14 -0300

    limpado projeto e criado header

---
+++ src/components/Cabecalho/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from './img/logo.png';
+import search from './img/search.png';
+import styles from './Cabecalho.module.scss';
+
+export default function Cabecalho() {
+  return (
+    <header className={styles.cabecalho}>
+      <img src={logo} alt="logo do alura space" />
+      <div className={styles.cabecalho__container}>
+        <input
+          className={styles.cabecalho__input}
+          type="text"
+          placeholder="O que você procura?"
+        />
+        <img src={search} alt="icone de lupa" />
+      </div>
+    </header>
+  )
+}
